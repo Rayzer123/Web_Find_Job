3e6f55c8e9fde2cbcd5ee61b2302fbd90814c9e1
Rayzer123 <144355505+Rayzer123@users.noreply.github.com>
2025-07-08 17:11:57 +0700

    Delete Web_Find_Job-main directory

--- Web_Find_Job-main/README.md
+++
@@ -1,2 +0,0 @@
-# Web_Find_Job
-This web will help people who want to hiring staff and who want to find place for work
--- Web_Find_Job-main/SEARCHJOP/db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'findjob');
-if(!$conn){
-    die("Kết nối database thất bại: " . mysqli_connect_error());
-}
-?>
